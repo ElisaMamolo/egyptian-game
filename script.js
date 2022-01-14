82707e8e22266ded12fbeedc451d4948aff45697
ElisaMamolo <elisa.carlotta.mamolo@lego.com>
2022-01-14 09:33:08 +0100

    now player starts at the bottom of canvas and moves + cleanup unused code

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ document.getElementById("startPlaying")
   //myGameArea.music();
   document.getElementById("startPlaying").remove();
   //start game area and create canvas when button is clicked
+  
   myGameArea.start();
 });
 
@@ -22,7 +23,7 @@ const myGameArea = {
     //insert canvas inside of the bdy as its first child
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     //call update game area every 20 seconds
-    //this.interval = setInterval(updateGameArea, 20);
+    this.interval = setInterval(updateGameArea, 20);
   },
   music: function () {
     var audio = new Audio('theme_song.mp3');
@@ -31,7 +32,7 @@ const myGameArea = {
   score: function () {
     const points = Math.floor(this.frames / 5);
     this.context.font = "18px serif";
-    this.context.fillStyle = "black";
+    this.context.fillStyle = "white";
     this.context.fillText(`Score: ${points}`, 350, 50);
   },
   clear: function () {
@@ -40,16 +41,104 @@ const myGameArea = {
   },
   stop: function () {
     clearInterval(this.interval);
-  },
-  
+  }
 };
 
+//create components, for our player element, and for the obstacles
+class Component {
+  constructor(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.color = color;
+    this.x = x;
+    this.y = y;
 
-//this gets called every 20 milliseconds
-function updateGameArea() {  
+    this.speedX = 0;
+    this.speedY = 0;
+  }
+
+  newPos() {
+    //give player a new position based on the speed
+    this.x += this.speedX;
+    this.y += this.speedY;
+  }
+
+  update() {
+    //take x,y, width and heigh and create whatever is in x and y
+    const ctx = myGameArea.context;
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+
+  left() {
+    return this.x;
+  }
+  right() {
+    return this.x + this.width;
+  }
+  top() {
+    return this.y;
+  }
+  bottom() {
+    return this.y + this.height;
+  }
+
+  crashWith(obstacle) {
+    //method for class component, we pass the object obstacle
+    //returns a boolean if we crashed or not
+    return !(
+      this.bottom() < obstacle.top() ||
+      this.top() > obstacle.bottom() ||
+      this.right() < obstacle.left() ||
+      this.left() > obstacle.right()
+    );
+  }
 }
 
+//create player from component class
+const player = new Component(20, 20, "red", 180, 380);
 
+//this gets called every 20 milliseconds
+function updateGameArea() {
+  //clear game area, the canvas
+  myGameArea.clear();
+  //give new position to the player
+  //update x and y with the speed
+  player.newPos();
+  //update component
+  player.update();
+  //update obstacle
+  this.updateObstacles();
+  //check if game over
+  checkGameOver();
+  // update and draw the score
+  myGameArea.score();
+}
 
+//update speed when arrows are clicked
+document.addEventListener("keydown", (e) => {
+  switch (e.keyCode) {
+    case 38: // up arrow
+      player.speedY -= 3;
+      break;
+    case 40: // down arrow
+      player.speedY += 3;
+      break;
+    case 37: // left arrow
+      player.speedX -= 3;
+      break;
+    case 39: // right arrow
+      player.speedX += 3;
+      break;
+  }
+});
+
+//We will also need a keyup function so we stop adding speed to our player,
+//otherwise, this won´t stop until another key is pressed. Let´s add it as well.
+document.addEventListener("keyup", (e) => {
+  //when nothing is moved speed is 0
+  player.speedX = 0;
+  player.speedY = 0;
+});
 
 
